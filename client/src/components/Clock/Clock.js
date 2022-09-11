29566c4687427053b250764eaba24a3105f31d37
vladan345 <vladanm087@gmail.com>
2022-09-11 18:05:22 +0200

    fixed timer and style

--- client/src/components/Clock/Clock.js
+++ client/src/components/Clock/Clock.js
@@ -1,66 +1,27 @@
 import React from "react";
 import "./Clock.css";
 import { useState, useEffect } from "react";
+import { showRemaining, name } from "../../utils/getDate";
 
 import Unit from "./Unit/Unit";
 
-import { getDate, finalArray } from "../../utils/getDate";
-
 export default function Clock() {
-  const [seconds, setSeconds] = useState("0" + 0);
-  const [minutes, setMinutes] = useState("0" + 0);
-  const [hours, setHours] = useState("0" + 0);
-  const [days, setDays] = useState("0" + 0);
-  console.log(finalArray);
-  //Generate data
-  //   const dataScroll = document.querySelector(".data-scroll");
-  //   const months = [
-  //     "Jan",
-  //     "Feb",
-  //     "Mar",
-  //     "Apr",
-  //     "May",
-  //     "Jun",
-  //     "Jul",
-  //     "Aug",
-  //     "Sep",
-  //     "Oct",
-  //     "Nov",
-  //     "Dec",
-  //   ];
+  const [time, setTime] = useState(["0" + 0, "0" + 0, "0" + 0, "0" + 0]);
 
-  //   const generateData = () => {
-  //     let htmlString = "";
-
-  //     for (let i = 0; i < birthdays.length; i++) {
-  //       let month = parseInt(birthdays[i].date[5] + birthdays[i].date[6]);
-  //       let day = birthdays[i].date[8] + birthdays[i].date[9];
-
-  //       htmlString += `<div class="row">
-  //     <p class="name">${birthdays[i].name}</p>
-  //     <div class="menu-date">
-  //     <p class="menu-month">${months[month - 1]}</p>
-  //     <p class="menu-day">${day}</p>
-  //     </div>
-  //   </div>`;
-  //     }
-  //     dataScroll.innerHTML = htmlString;
-
-  //     // const currentDate = new Date().toString().split(" ");
-  //     // const today = (document.querySelector(
-  //     //   ".date-number"
-  //     // ).innerHTML = `${currentDate[2]} ${currentDate[1]} ${currentDate[3]}`);
-  //   };
-  //   generateData();
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(showRemaining);
+    }, 1000);
+  }, [time]);
 
   return (
     <div className="Clock">
-      {/* <h1>It's {dataObject.name}'s birthday soon</h1> */}
+      <h1>It's {name}'s birthday soon</h1>
       <div className="timer">
-        <Unit unit="day" unitValue={days} />
-        <Unit unit="hour" unitValue={hours} />
-        <Unit unit="minute" unitValue={minutes} />
-        <Unit unit="second" unitValue={seconds} />
+        <Unit unit="day" unitValue={time[0]} />
+        <Unit unit="hour" unitValue={time[1]} />
+        <Unit unit="minute" unitValue={time[2]} />
+        <Unit unit="second" unitValue={time[3]} />
       </div>
     </div>
   );
--- client/src/utils/getDate.js
+++ client/src/utils/getDate.js
@@ -1,157 +1,177 @@
-export let finalArray = null;
-export const getDate = () => {
-  var year = new Date().getFullYear();
-  let birthdays = [
-    {
-      year: year,
-      name: "Tara",
-      date: `${year}-01-19T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Dusan(Avala)",
-      date: `${year}-02-27T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Natalija",
-      date: `${year}03-29T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Vladan",
-      date: `${year}-04-10T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Jana",
-      date: `${year}-08-01T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Filip",
-      date: `${year}-08-20T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Nixa",
-      date: `${year}-09-02T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Alex",
-      date: `${year}-10-12T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Tonic",
-      date: `${year}-10-21T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Flora",
-      date: `${year}-11-28T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Dimi",
-      date: `${year}-12-04T00:00:00`,
-    },
-    {
-      year: year,
-      name: "Dusan(Souly) and Toza",
-      date: `${year}-12-12T00:00:00`,
-    },
-  ];
+var year = new Date().getFullYear();
+let birthdays = [
+  {
+    year: year,
+    name: "Tara",
+    date: `${year}-01-19T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Dusan(Avala)",
+    date: `${year}-02-27T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Natalija",
+    date: `${year}03-29T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Vladan",
+    date: `${year}-04-10T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Jana",
+    date: `${year}-08-01T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Filip",
+    date: `${year}-08-20T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Nixa",
+    date: `${year}-09-02T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Alex",
+    date: `${year}-10-12T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Tonic",
+    date: `${year}-10-21T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Flora",
+    date: `${year}-11-28T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Dimi",
+    date: `${year}-12-04T00:00:00`,
+  },
+  {
+    year: year,
+    name: "Dusan(Souly) and Toza",
+    date: `${year}-12-12T00:00:00`,
+  },
+];
 
-  let dataObject = {
-    name: "",
-    newDate: null,
-  };
-
-  var counter = 0;
-  //IF COUNTER IS EQUALS TO NUMBER OF BIRTHDAYS, THAT MEANS
-  //THERE ARE NO BIRTHDAYS LEFT THIS YEAR.
-  //IN THAT CASE WE NEED TO CHANGE CURRENT YEAR SO THE PROGRAM
-  //CAN CHECK NEXT BIRTHDAY IN THE BEGGINING OF THE NEXT YEAR.
-  function getBirthday(array) {
-    loopDates(array);
-    if (counter === array.length) {
-      year++;
-      let newArray = changeYear([...array]);
-      loopDates(newArray);
-    }
-  }
-  getBirthday(birthdays);
-
-  //THIS CHECKS IF BIRTHAY HAS PASSED, IF YES, IT ADDS 1 TO COUNTER
-  //IF NOT, IT MEANS THAT BIRTHDAY IS NEXT UP.
-  function loopDates(dateArray) {
-    var today = new Date();
-    for (let i = 0; i < dateArray.length; i++) {
-      var checkDate = new Date(dateArray[i].date);
-      var choosenName = dateArray[i].name;
-      if (checkDate > today) {
-        dataObject.newDate = checkDate;
-        dataObject.name = choosenName;
-        break;
-      } else {
-        counter++;
-      }
-    }
-  }
-
-  //THIS FUNCTION MAKES NEW ARRAY WITH ALL DATES WITH NEW YEAR.
-  function changeYear(oldArray) {
-    for (let i = 0; i < oldArray.length; i++) {
-      var newDate = year + oldArray[i].date.slice(-15);
-      oldArray[i].date = newDate;
-      oldArray[i].year = year;
-    }
-    return oldArray;
-  }
-
-  var end = new Date(dataObject.newDate);
-
-  var second = 1000;
-  var minute = second * 60;
-  var hour = minute * 60;
-  var day = hour * 24;
-  var timer;
-
-  function showRemaining() {
-    var now = new Date();
-    var distance = end - now;
-
-    if (distance < 0) {
-      clearInterval(timer);
-      document.querySelector("h1").innerHTML = "Visit our site";
-      return;
-    }
-
-    var days = Math.floor(distance / day);
-    var hours = Math.floor((distance % day) / hour);
-    var minutes = Math.floor((distance % hour) / minute);
-    var seconds = Math.floor((distance % minute) / second);
-
-    addZero([days, hours, minutes, seconds]);
-  }
-  let finalArray;
-  function addZero(array) {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] < 10) {
-        array[i] = "0" + array[i];
-      }
-    }
-    finalArray = array;
-    console.log(finalArray);
-    //   setDays(array[0]);
-    //   setHours(array[1]);
-    //   setMinutes(array[2]);
-    //   setSeconds(array[3]);
-    // updateDisplay(array);
-  }
-
-  timer = setInterval(showRemaining, 1000);
+let dataObject = {
+  newDate: null,
 };
-getDate();
+export let name = "";
+
+var counter = 0;
+//IF COUNTER IS EQUALS TO NUMBER OF BIRTHDAYS, THAT MEANS
+//THERE ARE NO BIRTHDAYS LEFT THIS YEAR.
+//IN THAT CASE WE NEED TO CHANGE CURRENT YEAR SO THE PROGRAM
+//CAN CHECK NEXT BIRTHDAY IN THE BEGGINING OF THE NEXT YEAR.
+function getBirthday(array) {
+  loopDates(array);
+  if (counter === array.length) {
+    year++;
+    let newArray = changeYear([...array]);
+    loopDates(newArray);
+  }
+}
+getBirthday(birthdays);
+
+//THIS FUNCTION MAKES NEW ARRAY WITH ALL DATES WITH NEW YEAR.
+function changeYear(oldArray) {
+  for (let i in oldArray) {
+    var newDate = year + oldArray[i].date.slice(-15);
+    oldArray[i].date = newDate;
+    oldArray[i].year = year;
+  }
+  return oldArray;
+}
+
+//THIS CHECKS IF BIRTHAY HAS PASSED, IF YES, IT ADDS 1 TO COUNTER
+//IF NOT, IT MEANS THAT BIRTHDAY IS NEXT UP.
+function loopDates(dateArray) {
+  var today = new Date();
+  for (let i = 0; i < dateArray.length; i++) {
+    var checkDate = new Date(dateArray[i].date);
+    var choosenName = dateArray[i].name;
+    if (checkDate > today) {
+      dataObject.newDate = checkDate;
+      name = choosenName;
+      break;
+    } else {
+      counter++;
+    }
+  }
+}
+
+var end = new Date(dataObject.newDate);
+
+var second = 1000;
+var minute = second * 60;
+var hour = minute * 60;
+var day = hour * 24;
+
+export function showRemaining() {
+  console.log("fired");
+  var now = new Date();
+  var distance = end - now;
+
+  var days = Math.floor(distance / day);
+  var hours = Math.floor((distance % day) / hour);
+  var minutes = Math.floor((distance % hour) / minute);
+  var seconds = Math.floor((distance % minute) / second);
+
+  let timeArr = [days, hours, minutes, seconds];
+
+  for (let i in timeArr) {
+    if (timeArr[i] < 10) {
+      timeArr[i] = "0" + timeArr[i];
+    }
+  }
+  return timeArr;
+}
+//Generate data
+//   const dataScroll = document.querySelector(".data-scroll");
+//   const months = [
+//     "Jan",
+//     "Feb",
+//     "Mar",
+//     "Apr",
+//     "May",
+//     "Jun",
+//     "Jul",
+//     "Aug",
+//     "Sep",
+//     "Oct",
+//     "Nov",
+//     "Dec",
+//   ];
+
+//   const generateData = () => {
+//     let htmlString = "";
+
+//     for (let i = 0; i < birthdays.length; i++) {
+//       let month = parseInt(birthdays[i].date[5] + birthdays[i].date[6]);
+//       let day = birthdays[i].date[8] + birthdays[i].date[9];
+
+//       htmlString += `<div class="row">
+//     <p class="name">${birthdays[i].name}</p>
+//     <div class="menu-date">
+//     <p class="menu-month">${months[month - 1]}</p>
+//     <p class="menu-day">${day}</p>
+//     </div>
+//   </div>`;
+//     }
+//     dataScroll.innerHTML = htmlString;
+
+//     // const currentDate = new Date().toString().split(" ");
+//     // const today = (document.querySelector(
+//     //   ".date-number"
+//     // ).innerHTML = `${currentDate[2]} ${currentDate[1]} ${currentDate[3]}`);
+//   };
+//   generateData();
